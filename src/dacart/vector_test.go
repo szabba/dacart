38019d966f0245354adf13dfa9a000cebad68c49
Karol Marcjan <karol.marcjan@gmail.com>
2014-02-27 17:25:01 +0100

    Add dacart.TestCrossProduct

--- src/dacart/vector_test.go
+++ src/dacart/vector_test.go
@@ -37,3 +37,27 @@ func TestDotProduct(t *testing.T) {
 		)
 	}
 }
+
+func aCrossBGivesC(a, b, c Vector, t *testing.T) {
+
+	if a.Cross(b).Dot(c) != 1 {
+
+		t.Fatalf(
+			"%v.Cross(%v).Dot(%v) should be 1 not %f",
+			a, b, c, a.Cross(b).Dot(c),
+		)
+	}
+}
+
+func TestCrossProduct(t *testing.T) {
+
+	eX, eY, eZ := UnitVectors()
+
+	aCrossBGivesC(eX, eY, eZ, t)
+	aCrossBGivesC(eZ, eX, eY, t)
+	aCrossBGivesC(eY, eZ, eX, t)
+
+	aCrossBGivesC(eY, eX, eZ.Negate(), t)
+	aCrossBGivesC(eX, eZ, eY.Negate(), t)
+	aCrossBGivesC(eZ, eY, eX.Negate(), t)
+}
