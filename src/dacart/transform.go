723ee79698f73064b90d0a8bca18f2b814b2a68c
Karol Marcjan <karol.marcjan@gmail.com>
2014-02-27 18:48:07 +0100

    Add dacart.AxisAngleRotation

--- src/dacart/transform.go
+++ src/dacart/transform.go
@@ -4,6 +4,10 @@
 
 package main
 
+import (
+	"math"
+)
+
 // A Vector -> Vector transformation
 type Transform interface {
 	Transform(Vector) Vector
@@ -18,4 +22,26 @@ type Translation struct {
 func (t Translation) Transform(v Vector) Vector {
 
 	return t.By.Plus(v)
+}
+
+// A rotation transform
+type AxisAngleRotation struct {
+	Axis  Vector
+	Angle float64
+}
+
+// Rotates a vector by aar.Angle around an axis parallel to aar.Axis going
+// through the origin point
+func (aar AxisAngleRotation) Transform(v Vector) (vRot Vector) {
+
+	cos, sin := math.Cos(aar.Angle), math.Sin(aar.Angle)
+	unit := aar.Axis.Unit()
+
+	vRot = NewZeroVector()
+
+	vRot = vRot.Plus(v.Scale(cos))
+	vRot = vRot.Plus(unit.Cross(v).Scale(sin))
+	vRot = vRot.Plus(unit.Scale(unit.Dot(v) * (1 - cos)))
+
+	return vRot
 }
